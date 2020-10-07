a0416b394404a9182d6d278da3dd623f75225e2d
my <1616ramirez@gmail.com>
2020-10-07 16:55:46 -0700

    Initial check in of file using javadoc comment style with no code changes.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	private int total; 
+	  
+	  public AddingMachine () 
+	  {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal ()  // getter method to return the total 
+	  {
+	    return 0;
+	  }
+	  
+	  public void add (int value) //add method to add parameter to total variable
+	  {
+		  
+	  }
+
+	  public void subtract (int value) // subtract method subtracts parameter from total variable
+	  {
+		  
+	  }
+
+	  public String toString () //toString method returns a string
+	  {
+	    return "";
+	  }
+
+	  public void clear() // clear method should clear the machine
+	  {
+		  
+	  }
+	
+	
+}
